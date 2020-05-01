397a1cfe6c10e00552dc03c7df5cb3946458e36f
TAKUYA-KUSAKABE <t.kusakabe0429@gmail.com>
2020-05-01 12:16:02 +0900

    Modify ブロックの生成

--- javascripts/block.js
+++ javascripts/block.js
@@ -1,27 +1,22 @@
 // ブロック
+let blocks = [];
 let blockRowCount = 3;
 let blockColumnCount = 5;
 let blockWidth = 75;
 let blockHeight = 20;
-let blockPadding = 10;
-let blockOffsetTop = 30;
+let blockPadding = 10; // ブロック間の隙間
+let blockOffsetTop = 30; // ブロックの生成位置
 let blockOffsetLeft = 30;
-
-// ブロックの生成
-let blocks = [];
-for(let i=0; i < blockColumnCount; i++){
-  blocks[i] = [];
-  for(let k=0; k < blockRowCount; k++){
-    blocks[i][k] = {x: 0, y: 0};
-  }
-}
 
 // ブロックの描写
 function drawblocks(){
+  // 二次元配列を作成し、x, yの値を適宜選択する
   for(let i=0; i < blockColumnCount; i++){
+    blocks[i] = [];
     for(let k=0; k < blockRowCount; k++){
       let blockX = (i*(blockWidth + blockPadding)) + blockOffsetLeft;
       let blockY = (k*(blockHeight + blockPadding)) + blockOffsetTop;
+      blocks[i][k] = {x: 0, y: 0};
       blocks[i][k].x = blockX;
       blocks[i][k].y = blockY;
       ctx.beginPath();
@@ -32,3 +27,5 @@ function drawblocks(){
     }
   }
 }
+
+
